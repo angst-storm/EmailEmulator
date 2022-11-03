2644e0b8874ebf96a1e13b0d6917a8947acd0d04
angst-storm <serezha.kiprin@gmail.com>
2022-11-03 13:41:31 +0500

    new sender with commands

--- Sender/Program.cs
+++ Sender/Program.cs
@@ -1,23 +1,25 @@
 ﻿using Shared;
 
-const string server = "redpanda:9092";
-const string topic = "mails";
+const string server = "localhost:9092";
+const string group = "sender";
+const string commandsTopic = "commands";
+const string mailsTopic = "mails";
 
-var redPanda = new RedPanda(server);
+var redPanda = new RedPanda(server, group);
+redPanda.Subscribe(commandsTopic);
 
 var themes = Enumerable.Range(1, 50).ToArray();
 var random = new Random();
-var tasks = new List<Task>();
 
-for (var i = 0; i < 1000; i++)
+while (true)
 {
-    tasks.Add(redPanda.Produce(topic, new Mail
-    {
-        themes = themes.Where(t => random.Next(10) == 0).ToArray()
-    }));
-}
-
-foreach (var task in tasks)
-{
-    await task;
+    var command = redPanda.Consume().Message.Value;
+    var parameters = command.Split();
+    if (parameters.Length == 2)
+        if (parameters[0] == "send")
+            for (var i = 0; i < int.Parse(parameters[1]); i++)
+                redPanda.Produce(mailsTopic, new Mail
+                {
+                    themes = themes.Where(t => random.Next(10) == 0).ToArray()
+                });
 }
