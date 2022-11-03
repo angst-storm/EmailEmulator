04348994cc9284ae0091f017893e6a5c6aa5886e
angst-storm <serezha.kiprin@gmail.com>
2022-11-03 13:49:57 +0500

    new reader dont initialize users

--- Reader/Program.cs
+++ Reader/Program.cs
@@ -1,37 +1,28 @@
 ﻿using System.Text.Json;
 using Shared;
 
-const string connString = "Server=postgres;Port=5432;Database=postgres;User Id=postgres;Password=tglc1996;";
-const string server = "redpanda:9092";
-const string mails = "mails";
-const string clicks = "clicks";
+const string connString = "Server=localhost;Port=5432;Database=postgres;User Id=postgres;Password=tglc1996;";
+const string server = "localhost:9092";
+const string mailsTopic = "mails";
+const string clicksTopic = "clicks";
 
 var db = new DataBase(connString);
-var redPanda = new RedPanda(server);
-
-var random = new Random();
-
-redPanda.Subscribe(mails);
-db.InitializeTables();
-for (var i = 0; i < 50; i++)
-    db.AddUser($"user{i + 1}", new[] { random.Next(1, 51), random.Next(1, 51), random.Next(1, 51) });
 var users = db.GetUsers().ToArray();
+
+var redPanda = new RedPanda(server, "reader");
+redPanda.Subscribe(mailsTopic);
 
 while (true)
 {
-    var cr = redPanda.Consume();
-    var mail = JsonSerializer.Deserialize<Mail>(cr.Message.Value);
+    var mailJson = redPanda.Consume().Message.Value;
+    var mail = JsonSerializer.Deserialize<Mail>(mailJson);
     if (mail is null)
-        throw new Exception();
+        throw new ArgumentNullException(nameof(mail), "Mail is null");
     foreach (var user in users)
-    {
         if (mail.themes.Intersect(user.Item2).Any())
-        {
-            redPanda.Produce(clicks, new Click
+            redPanda.Produce(clicksTopic, new Click
             {
                 username = user.Item1,
                 mailThemes = mail.themes
             });
-        }
-    }
 }
